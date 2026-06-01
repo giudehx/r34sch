2711ea44e17a772019622a16fa51c8b82e0054da
giudehx <giudes.2011@gmail.com>
2026-06-01 22:16:37 +0200

    v1.0.2

--- r34sch.py
+++ r34sch.py
@@ -3,7 +3,7 @@ Documentation, License etc.
 
 @package r34sch
 '''
-import requests, random, os, time
+import requests, random, os, time, sys
 from bs4 import BeautifulSoup
 from curl_cffi import requests as c_requests
 from dotenv import load_dotenv
@@ -15,7 +15,7 @@ IMPERSONATE_OPTIONS = ["chrome", "edge", "safari", "chrome110", "chrome120"]
 URL="https://rule34.xxx"
 # OG Image scrapper by ClaustAI/Kiyopon
 def getsafe(url, *args,**kwargs):
-    retries, delay = 5, 2
+    retries, delay = 6, 2
     if "headers" not in kwargs:
         kwargs["headers"] = {}
     while retries > 0:
@@ -26,6 +26,8 @@ def getsafe(url, *args,**kwargs):
                 resp = s.get(url, *args, **kwargs)
                 if resp.status_code == 403:
                     raise c_requests.errors.RequestsError("403 blocked")
+                if resp.status_code == 429:
+                    raise c_requests.errors.RequestsError("429 too many requests")
                 return resp
             except (c_requests.errors.RequestsError, Exception) as e:
                 retries -= 1
@@ -35,19 +37,39 @@ def getsafe(url, *args,**kwargs):
                 time.sleep(delay)
                 delay *= 2
                 kwargs["impersonate"] = random.choice([b for b in IMPERSONATE_OPTIONS if b != c_imp])
+def download(urlf, out="r34out"):
+    print(f"--> downloading: {urlf}", end="")
+    if not os.path.exists(os.path.abspath(out)):
+        os.makedirs(os.path.abspath(out), exist_ok=True)
+    os.chdir(os.path.abspath(out))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        'Referer': URL
+    }
+    fname=urlf.split('/')[-1]
+    r = requests.get(urlf, stream=True, headers=headers); print(".", end="")
+    r.raise_for_status()
+    with open(fname,'wb') as f:
+        for c in r.iter_content(chunk_size=8192):
+            f.write(c)
+            print(".", end="")
+    print(" [ok]")
+    os.chdir("..")
+    time.sleep(0.25)
 def r34search(prompt:str,
               pid=1,
               hd_image=True,
-              images=10,
-              rand=True):
+              images=10):
     """
     Search R34 Content using a prompt.
     prompt (str): prompt to search (eg. foo_(bar))
     images (int): number of images to return, default is 5.
-    rand (bool): If set to True (default), it will randomize the images.
     hd_image (bool): If set to True (default) it will download the image from the post instead of the thumbnail, if the searching takes too much time, set this to False.
     pid (int): Page number.
     """
+    if pid == 0:
+        print("r34sch: error: page 0 is not allowed")
+        exit(1)
     npid=24*pid
     if npid == 24: npid = 0
     pid=npid
@@ -58,16 +80,34 @@ def r34search(prompt:str,
     if "Nobody here but us chickens!".lower() in str(check_404).lower().strip():
         print(f"r34sch: error: nothing found for {prompt}, try to reformulate the prompt. or make it yourself.")
         exit(1)
-    n_imgs = len(soup.select(".image-list span"))
-    if n_imgs < images:
-        print(f"r34sch: info: there are {n_imgs} images instead of {images}, sorry to dissapoint.")
-        images = n_imgs
+    # fancy url scraping
+    detected = []
+    while len(detected) < images:
+        if hd_image: links = soup.select('.image-list a')
+        else: links = soup.select('.image-list img')
+        if links == []: break
+        for a in links:
+            print(f"FOUND LINK --> {a['href']}")
+            if len(detected) >= images: break
+            if hd_image: detected.append(a['href'])
+            else: detected.append(a['src'])
+        if len(detected) >= images: break
+        if len(detected) < images:
+            pid     += 1
+            pid      = 24 * pid
+            url      = f'{URL}/index.php?page=post&s=list&tags={prompt}&pid={pid}'
+            response = c_requests.get(url, impersonate="chrome")
+            soup     = BeautifulSoup(response.text, 'html.parser')
+        print(detected)
+    if len(detected) < images:
+        print(f"r34sch: info: we've detected {len(detected)} images instead of {images}")
+        images = len(detected)
     img_src_array = []
     parsed = 1
     if hd_image:
-        for a in soup.select('.image-list a'):
+        for a in detected:
             if parsed == images+1: break
-            img_url = URL+a['href']
+            img_url = URL+a
             print(f"[{parsed}/{images}] r34sch: parsing: {img_url}")
             try:
                 # Image parsing
@@ -76,51 +116,34 @@ def r34search(prompt:str,
                 img_src = img_soup.select_one('.flexi img')['src']
                 img_src = img_src.replace(img_src[::-1].split('?')[0][::-1],'')[:-1]
                 img_src_array.append(img_src)
+                download(img_src)
             except:
                 # Video parsing
                 try:
-                    try:hd = {"Cookie": os.getenv("R34_COOKIE")}
-                    except:
-                        print("r34sch: error: no cookie found.\nYou can fix it by:\n\n\t1. create a .env file at the same directory as r34sch.\n\t2. In the .env file, type: R34_COOKIE=\"<YOUR R34 COOKIE TEXT>\"")
-                        exit(1)
+                    hd = {"Cookie": os.getenv("R34_COOKIE")}
                     video_r = getsafe(img_url, impersonate="chrome", headers=hd)
                     v_soup = BeautifulSoup(video_r.text, 'html.parser')
                     v_src = v_soup.select_one('.flexi video').select_one('source')['src']
                     v_src = v_src.replace(v_src[::-1].split('?')[0][::-1],'')[:-1]
+                    download(v_src)
                     img_src_array.append(v_src)
                 except Exception as e:
-                    print(f"r34sch: error: {e}")
-                    exit(1)
-            time.sleep(random.uniform(1, 2))
+                    print(f"r34sch: error: {e}, sorry!")
+                    images -= 1
+                    continue
+            time.sleep(random.uniform(0.5, 1.5))
             parsed += 1
     else:
-        for i in soup.select('.image-list img'):
-            img_src=i['src']
-            img_src = img_src.replace(img_src[::-1].split('?')[0][::-1],'')[:-1]
+        for i in detected:
+            if parsed == images+1: break
+            img_src = i.replace(i[::-1].split('?')[0][::-1],'')[:-1]
             img_src_array.append(img_src)
-    # mods
-    if rand:img_src_array=random.sample(img_src_array,len(img_src_array))
-    return  img_src_array
-
-def download(img_array:list, out="r34out"):
-    os.makedirs(out, exist_ok=True)
-    os.chdir(out)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-        'Referer': URL
-    }
-    for i in img_array:
-        fname=i.split('/')[-1]
-        r = requests.get(i, stream=True, headers=headers)
-        r.raise_for_status()
-        with open(fname,'wb') as f: f.write(r.content)
-        print(f"[{img_array.index(i)+1}/{len(img_array)}] downloaded: {fname} ({os.path.getsize(fname)} bytes)")
-        time.sleep(1)
-    print(f"\nall done, files are in:\n\t{os.getcwd()}")
+            download(img_src)
+            parsed += 1
+    # mods.. there were mods
+    return img_src_array
 
 prompt=input("Insert prompt (R34): ")
 nimg = int(input("How many images?: "))
 urls=r34search(prompt, hd_image=True, images=nimg)
-print("\n")
-download(urls)
 print("\n-- r34sch done, many thanks!! :D --")
